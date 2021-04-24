67321770e2cbae14412da41cf9bec27835c44350
MathildeLasseigne <mathilde.lasseigne@gmail.com>
2021-04-24 21:10:01 +0200

    Cree une banque ou mettre les properties des boutons

---
+++ src/controller/ToolsBarBanque.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.scene.control.CheckBox;
+
+/**
+ * Reuni la liste des booleans representant les options que l utilisateur peut afficher
+ */
+public class ToolsBarBanque {
+
+    /**
+     * Defini si la grille est visible.
+     * <br/>Initialise par le CuisineController
+     */
+    public static CheckBox isGrilleVisible;
+
+    /**
+     * Defini si il est possible de deplacer les meubles dans le plan.
+     * <br/>Initialise par le CuisineController
+     */
+    public static CheckBox isMeubleMovable;
+
+}
